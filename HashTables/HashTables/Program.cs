cbeedf596959169661a586f72b808d88b08b1a8b
ronit#123 <ronitwaje23>
2022-02-06 19:38:30 +0530

    hashtable program

---
+++ HashTables/HashTables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to hashtable program");
+            Console.ReadLine();
+        }
+    }
+}
